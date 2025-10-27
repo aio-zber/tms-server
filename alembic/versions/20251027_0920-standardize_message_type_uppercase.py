b59c9df185ce35d575e8c22171b579d7bb0d03d0
aio-zber <aiofficerzber.gcgc@gmail.com>
2025-10-27 12:44:51 +0800

    Fix Wrong Migration down_revision

--- alembic/versions/20251027_0920-standardize_message_type_uppercase.py
+++ alembic/versions/20251027_0920-standardize_message_type_uppercase.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'a1b2c3d4e5f6'
-down_revision: Union[str, None] = 'enable_pg_trgm_extension'
+down_revision: Union[str, None] = '1a2b3c4d5e6f'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
